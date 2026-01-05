2cc85c0e60c7a8a35eb640d8034127985d49ddfd
Ayush Ambatkar <ayush4yush@gmail.com>
2026-01-05 22:08:55 +0530

    update version

--- app/config.py
+++ app/config.py
@@ -13,7 +13,7 @@ class Settings(BaseSettings):
 
     # Application
     app_name: str = "Rate Limiter API"
-    app_version: str = "1.0.0"
+    app_version: str = "1.0.2"
     debug: bool = False
 
     # Redis Configuration
